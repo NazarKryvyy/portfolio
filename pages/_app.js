7010bc1254eb16d1dc8fc9feaacf193e6988f35a
NazarKryvyy <kryvyynazariy@gamil.com>
2021-06-25 15:19:54 +0300

    added apollo HOC

--- pages/_app.js
+++ pages/_app.js
@@ -6,28 +6,24 @@ import AppNavBar from "components/shared/NavBar";
 import Hero from "../components/shared/Hero";
 
 const MyApp = ({ Component, pageProps }) => {
-
-  const isHomePage = () => Component.name === 'Home'
+  const isHomePage = () => Component.name === "Home";
   return (
-      <div className="portfolio-app">
-        <AppNavBar />
-        {isHomePage() && <Hero />}
-        <div className="container">
-          <Component {...pageProps} />
-        </div>
-        {/* FOOTER STARTS */}
-        {isHomePage() && (
-          <footer
-            id="sticky-footer"
-            className="py-4 bg-black text-white-50 py-3"
-          >
-            <div className="container text-center">
-              <small>Copyright &copy; Your Website</small>
-            </div>
-          </footer>
-        )}
-        {/* FOOTER ENDS */}
+    <div className="portfolio-app">
+      <AppNavBar />
+      {isHomePage() && <Hero />}
+      <div className="container">
+        <Component {...pageProps} />
       </div>
+      {/* FOOTER STARTS */}
+      {isHomePage() && (
+        <footer id="sticky-footer" className="py-4 bg-black text-white-50 py-3">
+          <div className="container text-center">
+            <small>Copyright &copy; Your Website</small>
+          </div>
+        </footer>
+      )}
+      {/* FOOTER ENDS */}
+    </div>
   );
 };
 
--- pages/portfolios/[id].js
+++ pages/portfolios/[id].js
@@ -1,28 +1,24 @@
-import React, { useState, useEffect } from "react";
-import { useLazyQuery } from "@apollo/react-hooks";
+import React from "react";
+import { useQuery } from "@apollo/react-hooks";
 import { GET_PORTFOLIO } from "@/apollo/queries";
-import withApollo from '@/hoc/withApollo';
-import { getDataFromTree } from '@apollo/react-ssr';
+import withApollo from "@/hoc/withApollo";
+import { getDataFromTree } from "@apollo/react-ssr";
 
 const PortfolioDetail = ({ query }) => {
-  const [getPortfolio, { loading, data }] = useLazyQuery(GET_PORTFOLIO);
+  const { data } = useQuery(GET_PORTFOLIO, { variables: { id: query.id } });
 
-  useEffect(() => {
-    getPortfolio({ variables: { id: query.id } });
-  }, []);
-  if (loading || !data?.portfolio) {
-    return "Loading...";
-  }
+  const portfolio = (data && data.portfolio) || {};
+
   return (
     <div className="portfolio-detail">
       <div className="container">
         <div className="jumbotron">
-          <h1 className="display-3">{data.portfolio.title}</h1>
-          <p className="lead">{data.portfolio.jobTitle}</p>
+          <h1 className="display-3">{portfolio.title}</h1>
+          <p className="lead">{portfolio.jobTitle}</p>
           <p>
             <a
               className="btn btn-lg btn-success"
-              href={data.portfolio.companyWebsite}
+              href={portfolio.companyWebsite}
               role="button"
             >
               See Company
@@ -33,10 +29,10 @@ const PortfolioDetail = ({ query }) => {
         <div className="row marketing">
           <div className="col-lg-6">
             <h4 className="title">Location</h4>
-            <p className="text">{data.portfolio.location}</p>
+            <p className="text">{portfolio.location}</p>
 
             <h4 className="title">Start Date</h4>
-            <p className="text">{data.portfolio.startDate}</p>
+            <p className="text">{portfolio.startDate}</p>
           </div>
 
           <div className="col-lg-6">
@@ -45,12 +41,12 @@ const PortfolioDetail = ({ query }) => {
             <p className="text">44</p>
 
             <h4 className="title">End Date</h4>
-            <p className="text">{data.portfolio.endDate}</p>
+            <p className="text">{portfolio.endDate}</p>
           </div>
           <div className="col-md-12">
             <hr />
             <h4 className="title">Description</h4>
-            <p>{data.portfolio.description}</p>
+            <p>{portfolio.description}</p>
           </div>
         </div>
       </div>
@@ -62,4 +58,4 @@ PortfolioDetail.getInitialProps = async ({ query }) => {
   return { query };
 };
 
-export default withApollo(PortfolioDetail, {getDataFromTree});
+export default withApollo(PortfolioDetail, { getDataFromTree });
