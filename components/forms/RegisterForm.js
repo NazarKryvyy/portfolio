ffb754b199d671c8b6f48ea6a88de75262ee5881
NazarKryvyy <kryvyynazariy@gmail.com>
2021-11-13 22:26:11 +0200

    Add react-hook-form 66

--- components/forms/RegisterForm.js
+++ components/forms/RegisterForm.js
@@ -1,65 +1,52 @@
-import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 const RegisterForm = ({ onSubmit }) => {
-  const [form, setForm] = useState({});
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setForm({
-      ...form,
-      [name]: value,
-    });
-  };
+  const { register, handleSubmit } = useForm();
 
   return (
-    <form onSubmit={() => onSubmit(form)}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="form-group">
         <label htmlFor="avatar">Avatar</label>
         <input
-          onChange={handleChange}
+          {...register("avatar", { required: true })}
           type="text"
           className="form-control"
-          name="avatar"
           id="avatar"
         />
       </div>
       <div className="form-group">
         <label htmlFor="username">Username</label>
         <input
-          onChange={handleChange}
+          {...register("username", { required: true })}
           type="text"
           className="form-control"
-          name="username"
           id="username"
         />
       </div>
       <div className="form-group">
         <label htmlFor="email">Email</label>
         <input
-          onChange={handleChange}
+          {...register("email", { required: true })}
           type="email"
           className="form-control"
-          name="email"
           id="email"
         />
       </div>
       <div className="form-group">
         <label htmlFor="password">Password</label>
         <input
-          onChange={handleChange}
+          {...register("password")}
           type="password"
           className="form-control"
-          name="password"
           id="password"
         />
       </div>
       <div className="form-group">
         <label htmlFor="passwordConfirmation">Password Confirmation</label>
         <input
-          onChange={handleChange}
+          {...register("passwordConfirmation", { required: true })}
           type="password"
           className="form-control"
-          name="passwordConfirmation"
           id="passwordConfirmation"
         />
       </div>
