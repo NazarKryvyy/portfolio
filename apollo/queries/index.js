b626f053ebb8a2e1fd461af9dc379097e2826829
NazarKryvyy <kryvyynazariy@gmail.com>
2021-12-09 21:59:36 +0200

    Add login page 71

--- apollo/queries/index.js
+++ apollo/queries/index.js
@@ -114,5 +114,14 @@ export const SIGN_UP = gql`
     )
   }
 `;
-
+export const SIGN_IN = gql`
+  mutation SignIn($email: String!, $password: String!) {
+    signIn(input: { email: $email, password: $password }) {
+      _id
+      username
+      role
+      avatar
+    }
+  }
+`;
 // AUTH QUERIES END ----------------------------
