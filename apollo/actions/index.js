da25c2eb3536933331b2e3976e3065e02b970ac6
NazarKryvyy <kryvyynazariy@gmail.com>
2021-12-12 22:26:43 +0200

    Add withAuth HOC 77

--- apollo/actions/index.js
+++ apollo/actions/index.js
@@ -46,5 +46,6 @@ export const useSignIn = () =>
   });
 export const useSignOut = () => useMutation(SIGN_OUT);
 export const useLazyGetUser = () => useLazyQuery(GET_USER);
+export const useGetUser = () => useQuery(GET_USER);
 
 // Auth actions end -----------------------
