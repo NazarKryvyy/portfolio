5b195f3f0b5d0cc4aba17a990dacac5d1a62f57c
NazarKryvyy <kryvyynazariy@gmail.com>
2022-01-03 12:58:21 +0200

    Add get topics by slug 106

--- server/graphql/resolvers/index.js
+++ server/graphql/resolvers/index.js
@@ -54,7 +54,12 @@ exports.forumQueries = {
   forumCategories: (root, args, ctx) => {
     return ctx.models.ForumCategory.getAll();
   },
-  topicsByCategory: (root, { category }, ctx) => {
-    return ctx.models.Topic.getAllByCategory(category);
+  topicsByCategory: async (root, { category }, ctx) => {
+    const forumCategory = await ctx.models.ForumCategory.getBySlug(category);
+    if (!forumCategory) {
+      return null;
+    }
+
+    return ctx.models.Topic.getAllByCategory(forumCategory._id);
   },
 };
